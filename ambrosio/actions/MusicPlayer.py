da3b28803dff1e886601619ad225916d05e13d7f
joel <joelgodia@hotmail.com>
2016-04-19 15:46:48 +0200

    Created

---
+++ ambrosio/actions/MusicPlayer.py
@@ -0,0 +1,16 @@
+from action import action
+
+class MusicPlayer(Action):
+    """MusicPlayer for Ambrosio"""
+    def __init__(self):
+        super(MusicPlayer, self).__init__()
+        self.triggers = ["music", "audio"]
+
+    def do(self, command):
+        print "Will play music", " ".join(command)
+        return "OK"
+
+    def is_for_you(self, word):
+        if word in self.triggers:
+            return True
+        return False
